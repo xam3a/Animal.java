d89c631046285f028a2a581fa94b1d6566ef2df1
Hamza Zant <88774727+xam3a@users.noreply.github.com>
2022-05-25 09:20:25 +0200

    Create spider.java

---
+++ spider.java
@@ -0,0 +1,12 @@
+public class spider extends Animal {
+
+    public spider() {
+        super(8);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Spider eat inspects");
+    }
+
+}
